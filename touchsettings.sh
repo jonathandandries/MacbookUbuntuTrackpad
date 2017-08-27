5c8f06055089ed298fa1794fd118486e1500b047
Jonathan D'Andries <jonathandandries@gmail.com>
2017-08-27 12:00:10 -0500

    Adjusted for my preferences (e.g., no natural scroll)

---
+++ touchsettings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+synclient FingerLow=70
+synclient FingerHigh=75
+synclient EmulateTwoFingerMinZ=300
+synclient EmulateTwoFingerMinW=15
+# Disable natural scroll
+#synclient VertScrollDelta=-260
+#synclient HorizScrollDelta=-260
+synclient VertEdgeScroll=0
+synclient HorizTwoFingerScroll=1
+synclient TapButton1=0
+synclient TapButton2=0
+synclient TapButton3=0
+synclient ClickFinger3=2
+synclient PalmDetect=1
+synclient PalmMinWidth=14
+synclient PalmMinZ=200
+synclient CoastingSpeed=10
+synclient CoastingFriction=130
